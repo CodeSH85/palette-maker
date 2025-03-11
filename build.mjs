7df71ea7d96ac441312870ff4ce0604cdf38adbe
CodeSH85 <codesh85@gmail.com>
2025-03-11 21:27:16 +0800

    chore: Add esbuild bundle script in package.json

--- build.mjs
+++
@@ -1,9 +0,0 @@
-import * as esbuild from 'esbuild';
-
-await esbuild.build({
-	outdir: './dist',
-	entryPoints: ['./src/code.ts'],
-  bundle: true,
-	treeShaking: true,
-	target: 'es2019' // Figma support limited js features.
-})
