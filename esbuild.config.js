0b396747d89deb708feba477a56e8fd69e1dda94
CodeSH85 <codesh85@gmail.com>
2025-03-22 20:12:21 +0800

    chore: enhance build process with dynamic HTML and CSS injection

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,20 +1,70 @@
+import fs from 'node:fs';
 import * as esbuild from 'esbuild';
 
 const isWatchMode = process.argv.includes('--watch');
 
 const buildOptions = {
+	entryPoints: [
+		'./src/code.ts',
+		'./src/css/style.css',
+		"./ui.html"
+	],
   outdir: './dist',
-  entryPoints: ['./src/code.ts'],
+	minify: true,
   bundle: true,
   treeShaking: true,
+	// sourcemap: true,
+	assetNames: 'assets/[name]',
+	loader: {
+		".html": "file"
+	},
   target: 'es2016',
+	write: false
 };
 
-if (isWatchMode) {
-  const ctx = await esbuild.context(buildOptions);
-  await ctx.watch();
-  console.log("👀 Watching for changes...");
-} else {
-  await esbuild.build(buildOptions);
-  console.log("✅ Build complete!");
+async function build() {
+	const result = await esbuild.build(buildOptions);
+	const code = result.outputFiles.find(file => file.path.endsWith('code.js'))?.text;
+	const ui = result.outputFiles.find(file => file.path.endsWith('ui.js'))?.text;
+  const cssCode = result.outputFiles.find(file => file.path.endsWith('.css'))?.text || '';
+  const htmlCode = result.outputFiles.find(file => file.path.endsWith('.html'))?.text || '';
+
+	const styleTemplate = `<style>${cssCode}</style>`;
+	const htmlHeadEndIndex = htmlCode.indexOf('</head>');
+	const headStart = htmlCode.slice(0, htmlHeadEndIndex);
+	const headToEnd = htmlCode.slice(htmlHeadEndIndex, htmlCode.length - 1);
+	const injectedHtml = headStart.concat(styleTemplate).concat(headToEnd);
+
+	const scriptTemplateForUI = `<script>${ui}</script>`;
+	const scriptEndIndex = injectedHtml.indexOf('</body>');
+	const bodyStart = injectedHtml.slice(0, scriptEndIndex);
+	const bodyToEnd = injectedHtml.slice(scriptEndIndex, injectedHtml.length - 1);
+	const injectedHtmlForUI =
+		bodyStart
+			.concat(scriptTemplateForUI)
+			.concat(bodyToEnd);
+
+	// console.log(injectedHtmlForUI);
+
+	fs.writeFile('./dist/ui.html', injectedHtmlForUI, err => {
+		if (err) {
+			console.error(err);
+		}
+	});
+	fs.writeFile('./dist/code.js', code, err => {
+		if (err) {
+			console.error(err);
+		}
+	})
 }
+
+build();
+
+// if (isWatchMode) {
+//   const ctx = await esbuild.context(buildOptions);
+//   await ctx.watch()
+//   console.log("👀 Watching for changes...");
+// } else {
+//   await build();
+//   console.log("✅ Build complete!");
+// }
---
+++ src/ui.ts
@@ -0,0 +1,65 @@
+window.onmessage = (event) => {
+	const { name, content } = event.data.pluginMessage;
+	console.log(content);
+	if (name === "get-variable-collections") {
+		const { collections } = content;
+		const list = document.querySelector("#variables-collection");
+		list.innerHTML = "";
+		collections.forEach(({ name, id, variableIds }) => {
+			const li = document.createElement("li");
+			li.textContent = name;
+			li.setAttribute("id", `collection-${id}`);
+			li.onclick = () => {
+				parent.postMessage(
+					{
+						pluginMessage: {
+							type: "get-variable-group",
+							id,
+							variableIds
+						},
+					},
+					"*"
+				);
+			};
+			list.appendChild(li);
+		});
+	} else if (name === 'get-collection-variables') {
+		const { variables } = content;
+		const colorVariablesDiv = document.querySelector("#color-variables");
+		colorVariablesDiv.innerHTML = "";
+		variables.forEach(({ name, valuesByMode }) => {
+			const div = document.createElement("div");
+			div.textContent = `${name}: value`;
+			colorVariablesDiv.appendChild(div);
+		});
+	}
+	if (name === "create-element") {
+		const { tag, attribute, children, parentElement } = content;
+		console.log(tag, attribute, children);
+
+		const element = document.createElement(tag);
+
+		if (children) {
+			element.innerHTML = children;
+		}
+
+		if (!!(attribute && Object.keys(attribute).length)) {
+			for (const [key, value] of Object.entries(attribute)) {
+				if (key === "style") {
+					for (const [styleKey, styleValue] of Object.entries(value)) {
+						element.style[styleKey] = styleValue;
+					}
+				} else if (typeof value === "string") {
+					element.setAttribute(key, value);
+				}
+			}
+		}
+
+		// Mount to parent element
+		let parent;
+		if (parentElement) {
+			parent = document.querySelector(parentElement);
+		}
+		parent.appendChild(element);
+	}
+};
