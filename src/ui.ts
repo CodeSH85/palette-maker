2ff500ade58b897232555356f667a3d24c0dcdf9
B10809010 <tina96024@gmail.com>
2025-03-31 20:36:59 +0800

    fix: faCode for button

--- src/ui.ts
+++ src/ui.ts
@@ -1,14 +1,17 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 // import { far } from '@fortawesome/free-regular-svg-icons'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // library.add(fas, far, fab)
 library.add(faBarsStaggered)
+library.add(faCode)
 dom.watch();
 
 window.onmessage = (event) => {
 	const { name, content } = event.data.pluginMessage;
+	console.log('content:', content);
 	if (name === "get-variable-collections") {
 		const { collections } = content;
 		const list = document.querySelector("#variables-collection");
