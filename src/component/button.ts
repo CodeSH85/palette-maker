02410fd0b0fa4badc98f1fefb0c977f9148ed63e
B10809010 <tina96024@gmail.com>
2025-03-31 20:33:55 +0800

    fix: variant for button (style, size, outline)

--- src/component/button.ts
+++ src/component/button.ts
@@ -5,7 +5,9 @@ type ButtonProps = {
 	parentElement: string
 	label?: string | number
 	icon?: string
+	size?: 'sm' | 'md' | 'lg'
 	variant?: "filled" | "tonal" | "text"
+	outlined?: boolean
 	css?: string
 	style?: Record<string, string>
 	onClick?: () => void
@@ -16,8 +18,10 @@ export function Button(property: ButtonProps) {
 	const {
 		label,
 		icon,
+		size = 'md',
 		parentElement,
 		variant = "filled",
+		outlined,
 		style,
 		onClick,
 	} = property
@@ -34,35 +38,54 @@ export function Button(property: ButtonProps) {
 	}
 
 	// const buttonClass = variant ? `${variant}-button` : "filled-button";
+	const buttonSize = {
+		sm: '20px',
+		md: '28px',
+		lg: '36px'
+	}
 	const buttonStyle = {
 		filled: {
+			height: buttonSize[size],
 			background: 'rgba(49, 87, 44, 1)',
 			color: '#FFFFFFFF',
 			fontWeight: 500,
 			letterSpacing: '1px',
-			padding: '10px 16px',
+			padding: '0px 16px',
 			borderRadius: '8px',
 			transition: 'background 0.2s',
 			cursor: 'pointer',
 			border: 'none'
 		},
 		tonal: {
+			height: buttonSize[size],
 			background: 'rgba(49, 87, 44, 1)',
 			color: '#FFFFFFFF',
 			fontWeight: 500,
-			padding: '10px 16px',
+			padding: '0px 16px',
 			borderRadius: '8px',
 			transition: 'background 0.2s',
 			cursor: 'pointer'
 		},
 		text: {
+			height: buttonSize[size],
 			background: 'transparent',
 			color: 'rgba(49, 87, 44, 1)',
 			fontWeight: 500,
-			padding: '10px 16px',
+			padding: '0px 16px',
 			borderRadius: '8px',
 			transition: 'color 0.2s',
 			cursor: 'pointer'
+		},
+		outlined: {
+			height: buttonSize[size],
+			background: 'transparent',
+			color: 'rgba(49, 87, 44, 1)',
+			fontWeight: 500,
+			padding: '0px 16px',
+			borderRadius: '8px',
+			transition: 'color 0.2s',
+			cursor: 'pointer',
+			border: '1px solid rgba(49, 87, 44, 1)'
 		}
 	}
 
@@ -71,12 +94,22 @@ export function Button(property: ButtonProps) {
 	if (onClick) {
 		attribute.onClick = onClick
 	}
-	if (variant) {
-		attribute.style = buttonStyle[variant]
-	}
-	if (style) {
-		attribute.style = style;
-	}
+	// if (variant) {
+	// 	attribute.style = buttonStyle[variant]
+	// }
+	// if (style) {
+	// 	const currentStyle = attribute.style || {}
+	// 	attribute.style = {
+	// 		...currentStyle,
+	// 		style
+	// 	};
+	// }
+
+	attribute.style = {
+		...(outlined? buttonStyle['outlined'] : (buttonStyle[variant] || {})),
+		...(style || {}),
+		...(icon ? { width: buttonSize[size], padding: '0px' } : {})
+	};
 
 	const buttonObj = createEle({
 		tag: "button",
