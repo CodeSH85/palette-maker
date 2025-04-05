87d395e355face685a0fd54cf254ed4dac56f44f
CodeSH85 <codesh85@gmail.com>
2025-04-05 20:08:10 +0800

    refactor: clean up console.log statements

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -10,7 +10,6 @@ type PostMessage = {
  */
 export function postMessageToUI(messageObj: PostMessage): void {
 	const { name } = messageObj;
-	console.log(messageObj);
 	if (!name || typeof name !== "string") return;
 	figma.ui.postMessage(messageObj);
 }
