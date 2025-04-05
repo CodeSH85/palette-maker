27ff32164234ca39d6b611da602cfdd2b20b84b6
CodeSH85 <codesh85@gmail.com>
2025-04-05 20:07:52 +0800

    feat: get color variables only and add checkbox for picking target

--- src/ui.ts
+++ src/ui.ts
@@ -10,14 +10,13 @@ type Content = Record<string, unknown>;
 
 window.onmessage = (event) => {
 	const { name, content } = event.data.pluginMessage;
-	console.log('content:', content);
 	eventMap[name]?.(content);
 };
 
 const eventMap: Record<string, (arg0: Content) => void> = {
 	"get-variable-collections": getVariableCollections,
 	"get-collection-variables": getCollectionVariables,
-	"create-element": createElement,
+	"create-element": createElement
 };
 
 function getVariableCollections(content: Content): void {
@@ -27,7 +26,12 @@ function getVariableCollections(content: Content): void {
 	list.innerHTML = "";
 	collections.forEach(({ name, id, variableIds }) => {
 		const li = document.createElement("li");
-		li.textContent = name;
+		const checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		li.append(checkbox);
+		const label = document.createElement("span");
+		label.textContent = name;
+		li.append(label);
 		li.setAttribute("id", `collection-${id}`);
 		li.onclick = () => {
 			parent.postMessage(
@@ -46,15 +50,56 @@ function getVariableCollections(content: Content): void {
 }
 
 function getCollectionVariables(content: Content): void {
-  const { variables } = content as { variables: { name: string; valuesByMode: unknown }[] };
+  const { variables } = content as { variables: { name: string; resolvedType: string }[] };
   const colorVariablesDiv = document.querySelector("#color-variables");
   if (!colorVariablesDiv) return;
   colorVariablesDiv.innerHTML = "";
-  variables.forEach(({ name }) => {
-    const div = document.createElement("div");
-    div.textContent = `${name}: value`;
-    colorVariablesDiv.appendChild(div);
-  });
+	console.log(variables);
+	const table = getVariableGroup(variables.filter(({ resolvedType }) => resolvedType === "COLOR"));
+	console.log(table);
+	Object.entries(table).forEach(([key, value]) => {
+		const div = document.createElement("div");
+		const checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		const label = document.createElement("span");
+		label.textContent = key;
+		div.append(checkbox);
+		div.append(label);
+
+		if (Object.entries(value).length) {
+			Object.entries(value).forEach(([val, num]) => {
+				const subDiv = document.createElement("div");
+				subDiv.style = "padding-left: 1em;"
+				const subCheckbox = document.createElement("input");
+				subCheckbox.type = "checkbox";
+
+				const subLabel = document.createElement("span");
+				subLabel.textContent = `${val}`;
+				subDiv.append(subCheckbox);
+				subDiv.append(subLabel);
+				if (num.length) {
+					const numHolder = document.createElement("div");
+					numHolder.style = "padding-left: 1em; display: flex; flex-direction: column; width: 100%;";
+					num.forEach((item => {
+						const numDiv = document.createElement("div");
+						numDiv.style = "display: flex; align-items: center; width: 100%;";
+						const numCheckbox = document.createElement("input");
+						numCheckbox.type = "checkbox";
+						numDiv.append(numCheckbox);
+						const numLabel = document.createElement("div");
+						numLabel.textContent = item;
+						numLabel.style = "padding-left: 1em;"
+						numDiv.append(numLabel);
+						numHolder.append(numDiv);
+					}));
+					subDiv.append(numHolder);
+				}
+
+				div.append(subDiv);
+			})
+		}
+		colorVariablesDiv.appendChild(div);
+	});
 }
 
 function createElement(content: Content): void {
@@ -84,4 +129,21 @@ function createElement(content: Content): void {
 
   const parent = parentElement ? document.querySelector(parentElement) : null;
   parent?.appendChild(element);
+}
+
+function getVariableGroup(collections: unknown[]): Record<string, Record<string, string>> {
+	const table = collections.reduce((obj, item) => {
+		const getGroup = item.name.split("/");
+		const group = getGroup[0];
+		const val = getGroup[1];
+		const num = getGroup[getGroup.length - 1];
+		if (!obj[group]) obj[group] = {};
+		if (!obj[group][val]) {
+			obj[group][val] = []
+		} else {
+			obj[group][val].push(num);
+		}
+		return obj;
+	}, {})
+	return table;
 }
